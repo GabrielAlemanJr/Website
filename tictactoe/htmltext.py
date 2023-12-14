dfd926ea33206c13a1fc46036b883013da8e587d
Gabriel Aleman <Gabriel.Aleman.Jr@outlook.com>
2023-12-14 01:10:13 -0500

    Updated css

--- tictactoe/htmltext.py
+++ tictactoe/htmltext.py
@@ -1,10 +1,94 @@
 
+
 def htmlfile(board):
     html = """
     <!DOCTYPE html>
     <html>
+    <style>
+    @media(max-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:725px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+
+	@media(min-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:750px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+    </style>
     <body>
-    <h1>"""+board+""""
+    <h1>"""+board+"""
     </h1>
     </body>
     </html>
@@ -16,12 +100,87 @@ def htmlnoinfo():
     <!DOCTYPE html>
     <html>
     <style>
-    input[type=submit]{
-     text-align: center;
-     font-size: 16px;
-     margin: 6px 6px;
-      cursor: pointer;
-    }
+    @media(max-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:725px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+
+	@media(min-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:750px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
     </style>
     <body>
     <h1> Please input password or userid</h1>
@@ -30,7 +189,113 @@ def htmlnoinfo():
     <input type="text" id="user_id" name="user_id"></br>
     <lable for="password">Enter Password</lable>
     <input type="text" id="password" name="password"></br>
-    <input type="submit" value="Submit">
+    <input type="submit" value="Submit" value:"Log In">
+    </form>
+    <form action="http://gabriel-awsprojects.s3-website-us-east-1.amazonaws.com/">
+	<input type="submit" value="Main Page">
+	</form>
+
+
+</body>
+</html>
+    """
+    return html
+    
+def htmlwrongpassword():
+    html = """
+    <!DOCTYPE html>
+    <html>
+    <style>
+   @media(max-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:725px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+
+	@media(min-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:750px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+    </style>
+    <body>
+    <h1>Please input correct password</h1>
+	<form action="https://okwcbra4u6.execute-api.us-east-1.amazonaws.com/tictactoe/Tictactoe" method="get">
+    <lable for="user_id">Enter UserID</lable>
+    <input type="text" id="user_id" name="user_id"></br>
+    <lable for="password">Enter Password</lable>
+    <input type="text" id="password" name="password"></br>
+    <input type="submit" value="Submit" value:"Log In">
     </form>
     <form action="http://gabriel-awsprojects.s3-website-us-east-1.amazonaws.com/">
 	<input type="submit" value="Main Page">
@@ -45,15 +310,98 @@ def htmlnoinfo():
 def htmlinput(board,user_id,password):
     html = """
     <html>
+    <style>
+    @media(max-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:725px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+
+	@media(min-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:750px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+    </style>
     <body>
-    <h1>"""+board+""""
+    <h1>"""+board+"""
     </h1>
     <form action="https://okwcbra4u6.execute-api.us-east-1.amazonaws.com/tictactoe/Tictactoe" method="get">
     	<lable for="userinput"> Please select your move with number 1-9</lable>
         <input type="text" id="userinput" name="userinput"></br>
         <input type="hidden" id="user_id" name="user_id" value="""+user_id+""" >
         <input type="hidden" id="password" name="password" value="""+password+""" >
-        <input type="submit" name = "SUBMIT">
+        <input type="submit" name = "SUBMIT" value:"Submit Turn">
     </form>
     <form action="http://gabriel-awsprojects.s3-website-us-east-1.amazonaws.com/userlogin.html?">
     <input type = "submit" value = "Return to log in">
@@ -79,6 +427,89 @@ def htmlplayerwin(board,winner,user_id,password):
     if winner == 1:
         html = """
         <html>
+        <style>
+         @media(max-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:725px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+
+	@media(min-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:750px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+         </style>
         <body>
         <h1>"""+user_id+""" wins</h1>
     
@@ -89,7 +520,7 @@ def htmlplayerwin(board,winner,user_id,password):
         <input type="hidden" id="reset" name="reset" value = "true">
         <input type="hidden" id="user_id" name="user_id" value="""+user_id+""" >
         <input type="hidden" id="password" name="password" value="""+password+""" >
-        <input type="submit" name = "SUBMIT">
+        <input type="submit" name = "SUBMIT" value:"Play Again?">
         </form>
         <form action="http://gabriel-awsprojects.s3-website-us-east-1.amazonaws.com/userlogin.html?">
         <input type = "submit" value = "Return to log in">
@@ -103,6 +534,89 @@ def htmlplayerwin(board,winner,user_id,password):
     elif winner == 3:
         html = """
         <html>
+        <style>
+         @media(max-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:725px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+
+	@media(min-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:750px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+        </style>
         <body>
         <h1>Its a Tie</h1>
     
@@ -113,7 +627,7 @@ def htmlplayerwin(board,winner,user_id,password):
         <input type="hidden" id="reset" name="reset" value = "true">
         <input type="hidden" id="user_id" name="user_id" value="""+user_id+""" >
         <input type="hidden" id="password" name="password" value="""+password+""" >
-        <input type="submit" name = "SUBMIT">
+        <input type="submit" name = "SUBMIT" value:"Play Again?">
         </form>
         <form action="http://gabriel-awsprojects.s3-website-us-east-1.amazonaws.com/userlogin.html?">
         <input type = "submit" value = "Return to log in">
@@ -127,6 +641,89 @@ def htmlplayerwin(board,winner,user_id,password):
     else:
         html = """
         <html>
+        <style>
+        @media(max-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:725px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+
+	@media(min-width: 1000px){
+    	form{
+        width:750px;   
+        text-alignment:center;
+           margin:0 auto;
+           background:white;
+            padding-right:25px;
+        }
+        h1{
+        font-size: clamp(60px,3vw,100px);
+        text-align:center;
+        }
+        
+        input[type=submit]{
+         background-color:orange;
+         width: 100%;
+         text-align:center;
+         font-size: clamp(30px,3vw,40px);
+          margin:6px 10px;
+          cursor: pointer;
+         }
+         input[type=text]{
+   
+         width:750px;
+         text-align: left;
+         font-size: clamp(30px,3vw,40px);
+         margin:6px 10px;
+         cursor: pointer;
+         }
+         lable{
+         width: 300px;
+         display:block;
+         font-size: clamp(30px,3vw,40px);
+         margin: 6px 6px;
+          cursor: pointer;
+         }
+	body {
+  	background-color: lightgray;
+        }
+	}
+        </style>
         <body>
         <h1>The Bot wins</h1>
      
@@ -137,7 +734,7 @@ def htmlplayerwin(board,winner,user_id,password):
         <input type="hidden" id="reset" name="reset" value = "true">
         <input type="hidden" id="user_id" name="user_id" value="""+user_id+""" >
         <input type="hidden" id="password" name="password" value="""+password+""" >
-        <input type="submit" name = "SUBMIT">
+        <input type="submit" name = "SUBMIT" value:"Play Again?">
         </form>
         <form action="http://gabriel-awsprojects.s3-website-us-east-1.amazonaws.com/userlogin.html?">
         <input type = "submit" value = "Return to log in">
